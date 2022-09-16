27909763783066d5dfc88f2d39a9b63cd94fb36b
Appyouz <91145436+Appyouz@users.noreply.github.com>
2022-09-16 16:24:58 +0545

    named method of import and export

---
+++ src/previousLessons/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/816vWy-0AJL._AC_UL116_SR116,116_.jpg",
+    title: "Fairy Tale",
+    author: "Stephen King",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61Bdp7XZhDL._AC_UL116_SR116,116_.jpg",
+    title: "The Silent Patient",
+    author: "Alex Michaelides",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL._AC_UL210_SR195,210_.jpg",
+    title: "Verity",
+    author: "Colleen Hoover",
+  },
+];
